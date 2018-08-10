65d6e9e22f2c042f5b7ad514f4c1cb0f69943195
reedHam <reed.hambrook@gmail.com>
2018-08-09 21:36:28 -0600

    port change

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('/GPUFractal', function (req, res) {
     res.render('pages/GPUFractal', {title: "GPU Fractal"});
 });
 
-server.listen(3000, function() {
+server.listen(8080, function() {
     console.log('Listening on ' + server.address().port);
 });
